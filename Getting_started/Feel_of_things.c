812b841528a432377f53f3c44b283a84c3eec6d2
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-02-07 16:00:24 +0530

    This is just for fun!

---
+++ Getting_started/Feel_of_things.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Just for fun.*/
+main()
+{
+    int num;
+
+    printf("Enter a number");
+    scanf("%d", &num);
+    printf("Now I am letting you a secret...");
+    printf("You have just entered the number %d", num);
+}
