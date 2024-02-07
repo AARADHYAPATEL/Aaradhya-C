7a7a241ff74681d60d2997c6242fd67048d17e0a
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-02-07 16:48:45 +0530

    Create First_if.c

---
+++ The Decision Control Structure/First_if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*Demonstration of if statement*/
+main()
+{
+    int num;
+
+    printf("Enter a number less than 10");
+    scanf("%d", &num);
+
+    if(num <= 10)
+        printf("What an obedient servant you are!");
+}
