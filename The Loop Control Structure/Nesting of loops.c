0798fab248a0117fdf31135a364078b8ac31a08c
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-01 11:05:34 +0530

    Create Nesting of loops.c

---
+++ The Loop Control Structure/Nesting of loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*Demonstration of nested loops*/
+int main()
+{
+    int r, c, sum;
+    for (r = 1; r <= 3; r++) /*outer loop*/
+    {
+        for (c = 1; c <= 2; c++) /*inner loop*/
+        {
+            sum = r + c;
+            printf("r = %d c = %d sum = %d\n", r, c, sum);
+        }
+    }
+}
