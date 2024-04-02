c4522cb76a8d2aa3786726d51b1ddd166b0f8043
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-02 22:33:15 +0530

    Create Call by Value.c

---
+++ Functions/Call by Value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 20;
+
+    swapv(a, b);
+    printf("\na = %d b = %d", a, b);
+}
+
+swapv(int x, int y)
+{
+    int t;
+
+    t = x;
+    x = y;
+    y = t;
+
+    printf("\nx = %d y = %d", x, y);
+}
