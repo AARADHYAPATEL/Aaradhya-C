bf2e1874fe425b70cb6a6188721112f2a4823fad
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-03 20:05:16 +0530

    Create A simple program using array.c

---
+++ Arrays/A simple program using array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    float avg, sum = 0;
+    int i;
+    int marks[30]; /*array declaration*/
+
+    for(i = 0; i <= 29; i++)
+    {
+        printf("\nEnter marks");
+        scanf("%d", &marks[i]); /*store data in array*/
+    }
+
+    for (i = 0; i <= 29; i++)
+        sum = sum + marks[i]; /*read data from an array*/
+
+    avg = sum / 30;
+    printf("\nAverage marks = %f", avg);
+}
