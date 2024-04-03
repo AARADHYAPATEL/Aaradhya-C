a749363a5940c83fdd26b83b39688ff9536e329b
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-03 16:42:40 +0530

    Create External storage class.c

---
+++ Data Types Revisited/External storage class.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int i;
+int main()
+{
+    printf("\ni = %d", i);
+    increment();
+    increment();
+    decrement();
+    decrement();
+}
+
+increment()
+{
+    i = i + 1;
+    printf("\non incrementing i = %d", i);
+}
+
+decrement()
+{
+    i = i - 1;
+    printf("\non decrementing i = %d", i);
+}
