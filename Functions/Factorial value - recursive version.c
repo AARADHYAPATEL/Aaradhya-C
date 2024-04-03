f0f50b57f81f2d2ad9d0dad2f6e5eaa1bc93a45f
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-03 11:13:06 +0530

    Create Factorial value - recursive version.c

---
+++ Functions/Factorial value - recursive version.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a, fact;
+
+    printf("\nEnter any number");
+    scanf("%d", &a);
+
+    fact = rec(a);
+    printf("Factorial value = %d", fact);
+}
+
+rec(x)
+int x;
+{
+    int f;
+
+    if (x == 1)
+        return(1);
+    else
+        f = x * rec (x - 1);
+
+    return(f);
+}
