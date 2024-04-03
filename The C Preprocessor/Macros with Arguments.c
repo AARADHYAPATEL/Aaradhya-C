eeb6ff0156c8b0ecfd66e90eab972b0b84d850cb
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-04-03 19:29:06 +0530

    Create Macros with Arguments.c

---
+++ The C Preprocessor/Macros with Arguments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define AREA(x) (3.14 * x * x)
+int main()
+{
+    float r1 = 6.25, r2 = 2.5, a;
+
+    a = AREA(r1);
+    printf("\nArea of circle = %f", a);
+    a = AREA(r2);
+    printf("\nArea of circle = %f", a);
+}
